172a938fb4e7d945639257f26f30a46d7825e1a8
pedro martelletto <pedro@yubico.com>
2022-02-16 12:57:34 +0100

    adjust copyright as per feedback
    adjust copyright as per feedback from Brett Berger in
    https://github.com/Yubico/libfido2/issues/481#issuecomment-1040723756.

--- src/nfc_win.c
+++ src/nfc_win.c
@@ -1,15 +1,7 @@
 /*
  * Copyright (c) 2022 Micro Focus or one of its affiliates.
- * Licensed under BSD 2 - Clause Simplified License(the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at :
- * https://github.com/Yubico/libfido2/blob/master/LICENSE
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
  */
 
 #include <winscard.h>
